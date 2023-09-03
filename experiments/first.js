24bacd0b74f3d02582dba14869200c1513302b85
designhokage <93583164+designhokage@users.noreply.github.com>
2023-09-03 12:23:30 +0200

    second iteration
    used for loop to create a specific amount of shapes.
    made colors transparent to be more aesthetically pleasing
    set a range of sizes for the shapes

--- experiments/first.js
+++ experiments/first.js
@@ -1,16 +1,17 @@
 function setup() {
   createCanvas(innerWidth, innerHeight);
   background(34, 39, 46);
+  noLoop();
 }
 
 function draw() {
-  let x = random(width);
-  let y = random(height);
-  let w = random(width);
-  let h = random(height);
-
-  fill(random(255), random(255), random(255));
-
-  rect(x, y, w, h);
-  ellipse(x, y, w, h);
+  background(255);
+  for (let i = 0; i < 50; i++) {
+    let x = random(width);
+    let y = random(height);
+    let size = random(20, 100);
+    fill(random(255), random(255), random(255), 100);
+    rect(x, y, size, size);
+    ellipse(x, y, size, size);
+  }
 }
