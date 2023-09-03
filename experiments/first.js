bf6ec09b6603eb7062cc1839352ea274390bed15
designhokage <93583164+designhokage@users.noreply.github.com>
2023-09-03 12:47:29 +0200

    third iteration
    randomly choose between rectangles and ellipse.

--- experiments/first.js
+++ experiments/first.js
@@ -10,8 +10,16 @@ function draw() {
     let x = random(width);
     let y = random(height);
     let size = random(20, 100);
+
     fill(random(255), random(255), random(255), 100);
-    rect(x, y, size, size);
-    ellipse(x, y, size, size);
+    /*rect(x, y, size, size);
+    ellipse(x, y, size, size);*/
+
+    // got help from Chat GPT for these lines of code
+    if (random() > 0.5) {
+      rect(x, y, size, size);
+    } else {
+      ellipse(x, y, size, size);
+    }
   }
 }
