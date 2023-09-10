9615d8c64ebff1128b6165aed6b2cf73b5e91211
designhokage <93583164+designhokage@users.noreply.github.com>
2023-09-10 11:07:24 +0200

    Flow Field experiment

---
+++ experiments/flowfield.js
@@ -0,0 +1,59 @@
+// followed a a tutorial (https://youtu.be/sZBfLgfsvSk?si=aptQbMMsLQpayvK-) and made additions and changes to give a differet feel
+
+let particles = [];
+const num = 1000;
+
+const noiseScale = 0.01 / 5;
+let direction = 1; // Direction variable to control the flow direction
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  for (let i = 0; i < num; i++) {
+    // Give particles random position and colors
+    particles.push({
+      position: createVector(random(width), random(height)),
+      color: color(random(255), random(255), random(255)), // Random color
+    });
+  }
+
+  clear();
+}
+
+function draw() {
+  background(0, 30);
+  for (let i = 0; i < num; i++) {
+    let particle = particles[i];
+    stroke(particle.color);
+    point(particle.position.x, particle.position.y);
+    let n = noise(
+      particle.position.x * noiseScale,
+      particle.position.y * noiseScale,
+      frameCount * noiseScale * noiseScale
+    );
+
+    // Change direction of particles when the mouse is released
+    if (!mouseIsPressed) {
+      direction = -direction;
+    }
+
+    let a = TAU * n * direction; // calculates the angle for particle movement
+    particle.position.x += cos(a);
+    particle.position.y += sin(a);
+
+    // Reset particles position if it goes off the screen
+    if (!onScreen(particle.position)) {
+      particle.position.x = random(width);
+      particle.position.y = random(height);
+    }
+  }
+}
+
+// Resets the noise
+function mouseReleased() {
+  noiseSeed(millis());
+}
+
+// Checks if vector is within the canvas
+function onScreen(v) {
+  return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
+}
