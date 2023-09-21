4c043970a734d6ecbe3aedff6ae2bd92a73e39f2
designhokage <93583164+designhokage@users.noreply.github.com>
2023-09-21 11:46:27 +0200

    Flow field variation with random color palettes

--- experiments/flowfield2.js
+++ experiments/flowfield2.js
@@ -7,13 +7,48 @@ const synth = new Tone.Synth().toDestination();
 const noiseScale = 0.01 / 5;
 let direction = 1; // Direction variable to control the flow direction
 
+const palettes = [
+  [
+    "#FF6B6B",
+    "#FFD166",
+    "#06D6A0",
+    "#277DA1",
+    "#7A5C61",
+    "#FDA7DF",
+    "#5E239D",
+    "#89B0AE",
+  ],
+  [
+    "#FF7A00",
+    "#FFD000",
+    "#8AC926",
+    "#1982C4",
+    "#6A0572",
+    "#AB83A1",
+    "#E7ECEF",
+    "#FF5A5F",
+  ],
+  [
+    "#8A4F7D",
+    "#9E768F",
+    "#C6A0A4",
+    "#F4E1D2",
+    "#E0ADA1",
+    "#8E8D8A",
+    "#74535D",
+    "#B7C3F3",
+  ],
+];
+
 function setup() {
   createCanvas(innerWidth, innerHeight);
+  const palette = random(palettes);
   for (let i = 0; i < num; i++) {
     // Give particles random position and colors
+    const randomColor = random(palette);
     particles.push({
       position: createVector(random(width), random(height)),
-      color: color(random(255), random(255), random(255)), // Random color
+      color: color(randomColor), // Random color
       radius: random(2, 6),
     });
   }
@@ -22,7 +57,7 @@ function setup() {
 }
 
 function draw() {
-  background(0, 10);
+  background(0, 20);
   for (let i = 0; i < num; i++) {
     let particle = particles[i];
     noStroke();
